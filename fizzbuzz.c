3371251bb64b5dc14563b1dcb7391cd05217232c
U-chrknk\spiral272 <spiral272@chrknk.(none)>
2014-04-07 16:37:26 +0900

    create fizzbuzz

---
+++ fizzbuzz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 1; ;i++){
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz\n");
+        else if (i % 3 == 0)
+            printf("FIzz\n");
+        else if (i % 5 == 0)
+            printf("Buzz\n");
+        else
+            printf("%d\n",i);
+    }
+
+    return 0;
+}
